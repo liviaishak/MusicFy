ac3d271e37892f38ed94df98cd4826b118bf88fe
Livia Ishak <liviaishak1@gmail.com>
2018-01-28 23:38:15 -0800

    assignment8: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -53,6 +53,15 @@ class Album extends Component {
     this.play(newSong);
   }
 
+//assignment8
+
+ handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = currentIndex + 1;
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
 
   render() {
     return (
@@ -96,6 +105,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
