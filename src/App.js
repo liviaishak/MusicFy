161d80779c8abd31cb49b98757d706673a2fc44b
Livia <liviaishak1@gmail.com>
2018-05-11 11:53:03 -0700

    renamed repo

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <Link to='/'>Landing</Link>
           <Link to='/library'>Library</Link>
         </nav>
-         <h1>Bloc Jams</h1>
+         <h1>MusicFy</h1>
       </header>
       <main>
         <Route exact path="/" component={Landing} />
